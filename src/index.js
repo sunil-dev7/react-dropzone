sunil-dev7/react-dropzone

commit 68257f2929ce2380f18843eec928c760f1c01112

fix: only call *onDrag callbacks when dragging files (#619)
BREAKING CHANGE:  Callbacks won't get executed for non-file items anymore i.e. if items aren't of type `File` when react-dropzone will ignore them.

src/index.js
@@ -10,7 +10,8 @@ import {
   fileMatchSize,
   onDocumentDragOver,
   getDataTransferItems as defaultGetDataTransferItem,
-  isIeOrEdge
+  isIeOrEdge,
+  hasFiles
 } from './utils'
 import styles from './utils/styles'
 
@@ -84,9 +85,11 @@ class Dropzone extends React.Component {
   }
 
   onDragStart(evt) {
-    if (this.props.onDragStart) {
-      this.props.onDragStart.call(this, evt)
-    }
+    Promise.resolve(this.props.getDataTransferItems(evt)).then(draggedFiles => {
+      if (hasFiles(draggedFiles) && this.props.onDragStart) {
+        this.props.onDragStart.call(this, evt)
+      }
+    })
   }
 
   onDragEnter(evt) {
@@ -100,14 +103,17 @@ class Dropzone extends React.Component {
     evt.persist()
 
     Promise.resolve(this.props.getDataTransferItems(evt)).then(draggedFiles => {
-      this.setState({
-        isDragActive: true, // Do not rely on files for the drag state. It doesn't work in Safari.
-        draggedFiles
-      })
+      if (hasFiles(draggedFiles)) {
+        this.setState({
+          isDragActive: true, // Do not rely on files for the drag state. It doesn't work in Safari.
+          draggedFiles
+        })
+
+        if (this.props.onDragEnter) {
+          this.props.onDragEnter.call(this, evt)
+        }
+      }
     })
-    if (this.props.onDragEnter) {
-      this.props.onDragEnter.call(this, evt)
-    }
   }
 
   onDragOver(evt) {
@@ -123,9 +129,12 @@ class Dropzone extends React.Component {
       // continue regardless of error
     }
 
-    if (this.props.onDragOver) {
-      this.props.onDragOver.call(this, evt)
-    }
+    Promise.resolve(this.props.getDataTransferItems(evt)).then(draggedFiles => {
+      if (hasFiles(draggedFiles) && this.props.onDragOver) {
+        this.props.onDragOver.call(this, evt)
+      }
+    })
+
     return false
   }
 
@@ -144,9 +153,11 @@ class Dropzone extends React.Component {
       draggedFiles: []
     })
 
-    if (this.props.onDragLeave) {
-      this.props.onDragLeave.call(this, evt)
-    }
+    Promise.resolve(this.props.getDataTransferItems(evt)).then(draggedFiles => {
+      if (hasFiles(draggedFiles) && this.props.onDragLeave) {
+        this.props.onDragLeave.call(this, evt)
+      }
+    })
   }
 
   onDrop(evt) {
@@ -210,7 +221,7 @@ class Dropzone extends React.Component {
         rejectedFiles.push(...acceptedFiles.splice(0))
       }
 
-      if (onDrop) {
+      if (hasFiles(fileList) && onDrop) {
         onDrop.call(this, acceptedFiles, rejectedFiles, evt)
       }
 

src/utils/index.js
@@ -15,7 +15,11 @@ export function getDataTransferItems(event) {
     } else if (dt.items && dt.items.length) {
       // During the drag even the dataTransfer.files is null
       // but Chrome implements some drag store, which is accesible via dataTransfer.items
-      dataTransferItemsList = dt.items
+      // Map the items to File objects,
+      // and filter non-File items
+      // see https://developer.mozilla.org/en-US/docs/Web/API/DataTransferItem/getAsFile
+      const files = Array.prototype.map.call(dt.items, item => item.getAsFile())
+      dataTransferItemsList = Array.prototype.filter.call(files, file => file !== null)
     }
   } else if (event.target && event.target.files) {
     dataTransferItemsList = event.target.files
@@ -37,6 +41,16 @@ export function fileMatchSize(file, maxSize, minSize) {
 
 export function allFilesAccepted(files, accept) {
   return files.every(file => fileAccepted(file, accept))
+}
+
+export function hasFiles(files) {
+  // Allow only files and retun the items as a list of File,
+  // see https://developer.mozilla.org/en-US/docs/Web/API/DataTransferItem for details
+  return (
+    Array.isArray(files) &&
+    files.length > 0 &&
+    Array.prototype.every.call(files, file => file instanceof File)
+  )
 }
 
 // allow the entire document to be a drag target

src/index.spec.js
@@ -15,7 +15,18 @@ const flushPromises = wrapper =>
 const Dropzone = require('./index')
 const DummyChildComponent = () => null
 
+const createFile = (name, size, type) => {
+  const file = new File([], name, { type })
+  Object.defineProperty(file, 'size', {
+    get() {
+      return size
+    }
+  })
+  return file
+}
+
 let files
+let nonFileItems
 let images
 
 const rejectColor = 'red'
@@ -33,26 +44,19 @@ const acceptStyle = {
 
 describe('Dropzone', () => {
   beforeEach(() => {
-    files = [
+    files = [createFile('file1.pdf', 1111, 'application/pdf')]
+
+    nonFileItems = [
       {
-        name: 'file1.pdf',
-        size: 1111,
-        type: 'application/pdf'
+        kind: 'string',
+        type: 'text/plain',
+        getAsFile() {
+          return null
+        }
       }
     ]
 
-    images = [
-      {
-        name: 'cats.gif',
-        size: 1234,
-        type: 'image/gif'
-      },
-      {
-        name: 'dogs.jpg',
-        size: 2345,
-        type: 'image/jpeg'
-      }
-    ]
+    images = [createFile('cats.gif', 1234, 'image/gif'), createFile('dogs.gif', 2345, 'image/jpeg')]
   })
 
   describe('basics', () => {
@@ -298,8 +302,8 @@ describe('Dropzone', () => {
     })
   })
 
-  describe('drag-n-drop', () => {
-    it('should override onDrag* methods', () => {
+  describe('drag-n-drop', async () => {
+    it('should override onDrag* methods', async () => {
       const props = {
         onDragStart: jest.fn(),
         onDragEnter: jest.fn(),
@@ -307,17 +311,25 @@ describe('Dropzone', () => {
         onDragLeave: jest.fn()
       }
       const component = mount(<Dropzone {...props} />)
-      component.simulate('dragStart')
+
+      component.simulate('dragStart', { dataTransfer: { files } })
+      await flushPromises(component)
       expect(props.onDragStart).toHaveBeenCalled()
-      component.simulate('dragEnter', { dataTransfer: { items: files } })
+
+      await component.simulate('dragEnter', { dataTransfer: { files } })
+      await flushPromises(component)
       expect(props.onDragEnter).toHaveBeenCalled()
-      component.simulate('dragOver', { dataTransfer: { items: files } })
+
+      await component.simulate('dragOver', { dataTransfer: { files } })
+      await flushPromises(component)
       expect(props.onDragOver).toHaveBeenCalled()
-      component.simulate('dragLeave', { dataTransfer: { items: files } })
+
+      await component.simulate('dragLeave', { dataTransfer: { files } })
+      await flushPromises(component)
       expect(props.onDragLeave).toHaveBeenCalled()
     })
 
-    it('should guard dropEffect in onDragOver for IE', () => {
+    it('should guard dropEffect in onDragOver for IE', async () => {
       const props = {
         onDragStart: jest.fn(),
         onDragEnter: jest.fn(),
@@ -326,42 +338,86 @@ describe('Dropzone', () => {
       const component = mount(<Dropzone {...props} />)
 
       // Using Proxy we'll emulate IE throwing when setting dataTransfer.dropEffect
-      const eventProxy = new Proxy(
-        {},
-        {
-          get: (target, prop) => {
-            switch (prop) {
-              case 'dataTransfer':
-                throw new Error('IE does not support rrror')
-              default:
-                return function noop() {}
+      const eventProxy = {
+        preventDefault() {},
+        stopPropagation() {},
+        dataTransfer: new Proxy(
+          {},
+          {
+            set: (target, prop) => {
+              switch (prop) {
+                case 'dropEffect':
+                  throw new Error('IE does not support setting {dropEffect}')
+                default:
+                  break
+              }
             }
           }
-        }
-      )
+        )
+      }
 
       // And using then we'll call the onDragOver with the proxy instead of event
-      const componentOnDragOver = component.instance().onDragOver
+      const instance = component.instance()
+      const componentOnDragOver = instance.onDragOver
       const onDragOver = jest
-        .spyOn(component.instance(), 'onDragOver')
+        .spyOn(instance, 'onDragOver')
         .mockImplementation(() => componentOnDragOver(eventProxy))
 
-      component.simulate('dragStart', { dataTransfer: { items: files } })
+      component.simulate('dragStart', { dataTransfer: { files } })
+      await flushPromises(component)
       expect(props.onDragStart).toHaveBeenCalled()
-      component.simulate('dragEnter', { dataTransfer: { items: files } })
+
+      component.simulate('dragEnter', { dataTransfer: { files } })
+      await flushPromises(component)
       expect(props.onDragEnter).toHaveBeenCalled()
-      component.simulate('dragLeave', { dataTransfer: { items: files } })
+
+      component.simulate('dragLeave', { dataTransfer: { files } })
+      await flushPromises(component)
       expect(props.onDragLeave).toHaveBeenCalled()
+
       // It should not throw the error
-      component.simulate('dragOver', { dataTransfer: { items: files } })
+      component.simulate('dragOver', { dataTransfer: { files } })
+      await flushPromises(component)
       expect(onDragOver).not.toThrow()
     })
 
-    it('should set proper dragActive state on dragEnter', async () => {
-      const dropzone = mount(<Dropzone>{props => <DummyChildComponent {...props} />}</Dropzone>)
-      dropzone.simulate('dragEnter', { dataTransfer: { files } })
+    it('should not call onDrag* if there are no files', async () => {
+      const props = {
+        onDragStart: jest.fn(),
+        onDragEnter: jest.fn(),
+        onDragOver: jest.fn(),
+        onDragLeave: jest.fn(),
+        onDrop: jest.fn()
+      }
 
-      const updatedDropzone = await flushPromises(dropzone)
+      const component = mount(<Dropzone {...props} />)
+
+      component.simulate('dragStart', { dataTransfer: { items: nonFileItems } })
+      await flushPromises(component)
+      expect(props.onDragStart).not.toHaveBeenCalled()
+
+      component.simulate('dragEnter', { dataTransfer: { items: nonFileItems } })
+      await flushPromises(component)
+      expect(props.onDragEnter).not.toHaveBeenCalled()
+
+      component.simulate('dragOver', { dataTransfer: { items: nonFileItems } })
+      await flushPromises(component)
+      expect(props.onDragOver).not.toHaveBeenCalled()
+
+      component.simulate('dragLeave', { dataTransfer: { items: nonFileItems } })
+      await flushPromises(component)
+      expect(props.onDragLeave).not.toHaveBeenCalled()
+
+      component.simulate('drop', { dataTransfer: { items: nonFileItems } })
+      await flushPromises(component)
+      expect(props.onDrop).not.toHaveBeenCalled()
+    })
+
+    it('should set proper dragActive state on dragEnter', async () => {
+      const component = mount(<Dropzone>{props => <DummyChildComponent {...props} />}</Dropzone>)
+      component.simulate('dragEnter', { dataTransfer: { files } })
+
+      const updatedDropzone = await flushPromises(component)
       const child = updatedDropzone.find(DummyChildComponent)
 
       expect(child).toHaveProp('isDragActive', true)
@@ -602,7 +658,7 @@ describe('Dropzone', () => {
       expect(dragActiveChild).toHaveProp('isDragAccept', true)
       expect(dragActiveChild).toHaveProp('isDragReject', false)
 
-      dropzone.simulate('dragLeave', { dataTransfer: { files } })
+      await dropzone.simulate('dragLeave', { dataTransfer: { files } })
       expect(dropzone.find(DragActiveComponent).children()).toHaveLength(0)
       expect(dropzone.find(ChildComponent)).toHaveProp('isDragAccept', false)
       expect(dropzone.find(ChildComponent)).toHaveProp('isDragReject', false)
@@ -765,13 +821,7 @@ describe('Dropzone', () => {
           accept="image/*"
         />
       )
-      const bogusImages = [
-        {
-          name: 'bogus.gif',
-          size: 1234,
-          type: 'application/x-moz-file'
-        }
-      ]
+      const bogusImages = [createFile('bogus.gif', 1234, 'application/x-moz-file')]
 
       await dropzone.simulate('drop', { dataTransfer: { files: bogusImages } })
       expect(onDrop).toHaveBeenCalledWith(bogusImages, [], expectedEvent)

src/utils/index.spec.js
@@ -1,4 +1,4 @@
-import { getDataTransferItems, isIeOrEdge } from './'
+import { getDataTransferItems, isIeOrEdge, hasFiles } from './'
 
 const files = [
   {
@@ -15,6 +15,33 @@ const files = [
     name: 'dogs.jpg',
     size: 2345,
     type: 'image/jpeg'
+  }
+]
+
+const nonFileItems = [
+  {
+    kind: 'string',
+    type: 'text/plain',
+    getAsFile() {
+      return null
+    }
+  }
+]
+
+const json = JSON.stringify({
+  ping: true
+})
+const file = new File([json], 'test.json', {
+  type: 'application/json'
+})
+
+const fileItems = [
+  {
+    kind: 'file',
+    type: 'application/json',
+    getAsFile() {
+      return file
+    }
   }
 ]
 
@@ -45,12 +72,26 @@ describe('getDataTransferItems', () => {
         files: [{}]
       },
       dataTransfer: {
-        items: files
+        items: fileItems
       }
     }
     const res = getDataTransferItems(event)
     expect(res).toBeInstanceOf(Array)
-    expect(res).toHaveLength(3)
+    expect(res).toHaveLength(1)
+  })
+
+  it('should ignore dataTransfer.items that are not of kind "file"', () => {
+    const event = {
+      target: {
+        files: [{}]
+      },
+      dataTransfer: {
+        items: nonFileItems
+      }
+    }
+    const res = getDataTransferItems(event)
+    expect(res).toBeInstanceOf(Array)
+    expect(res).toHaveLength(0)
   })
 
   it('should use event.target if dataTransfer is not defined', () => {
@@ -64,7 +105,7 @@ describe('getDataTransferItems', () => {
     expect(res).toHaveLength(3)
   })
 
-  it('should prioritize dataTransfer.files over .files', () => {
+  it('should prioritize dataTransfer.files over .items', () => {
     const event = {
       dataTransfer: {
         files: [{}, {}],
@@ -114,5 +155,14 @@ describe('isIeOrEdge', () => {
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
 
     expect(isIeOrEdge(userAgent)).toBe(false)
+  })
+})
+
+describe('hasFiles', () => {
+  it('should only return true for an Array of File objects', () => {
+    expect(hasFiles([file])).toBe(true)
+    expect(hasFiles(['domNode'])).toBe(false)
+    expect(hasFiles([])).toBe(false)
+    expect(hasFiles(null)).toBe(false)
   })
 })

commit 5c505b0b61e657e07138c84cdf61d21500b0ae3e

fix: fix a regression introduced by #619 (#678)
Closes https://github.com/react-dropzone/react-dropzone/pull/619#issuecomment-426573278

src/index.js
@@ -11,7 +11,7 @@ import {
   onDocumentDragOver,
   getDataTransferItems as defaultGetDataTransferItem,
   isIeOrEdge,
-  hasFiles
+  isFileList
 } from './utils'
 import styles from './utils/styles'
 
@@ -86,7 +86,7 @@ class Dropzone extends React.Component {
 
   onDragStart(evt) {
     Promise.resolve(this.props.getDataTransferItems(evt)).then(draggedFiles => {
-      if (hasFiles(draggedFiles) && this.props.onDragStart) {
+      if (isFileList(draggedFiles) && this.props.onDragStart) {
         this.props.onDragStart.call(this, evt)
       }
     })
@@ -103,7 +103,7 @@ class Dropzone extends React.Component {
     evt.persist()
 
     Promise.resolve(this.props.getDataTransferItems(evt)).then(draggedFiles => {
-      if (hasFiles(draggedFiles)) {
+      if (isFileList(draggedFiles)) {
         this.setState({
           isDragActive: true, // Do not rely on files for the drag state. It doesn't work in Safari.
           draggedFiles
@@ -130,7 +130,7 @@ class Dropzone extends React.Component {
     }
 
     Promise.resolve(this.props.getDataTransferItems(evt)).then(draggedFiles => {
-      if (hasFiles(draggedFiles) && this.props.onDragOver) {
+      if (isFileList(draggedFiles) && this.props.onDragOver) {
         this.props.onDragOver.call(this, evt)
       }
     })
@@ -154,7 +154,7 @@ class Dropzone extends React.Component {
     })
 
     Promise.resolve(this.props.getDataTransferItems(evt)).then(draggedFiles => {
-      if (hasFiles(draggedFiles) && this.props.onDragLeave) {
+      if (isFileList(draggedFiles) && this.props.onDragLeave) {
         this.props.onDragLeave.call(this, evt)
       }
     })
@@ -221,7 +221,7 @@ class Dropzone extends React.Component {
         rejectedFiles.push(...acceptedFiles.splice(0))
       }
 
-      if (hasFiles(fileList) && onDrop) {
+      if (isFileList(fileList) && onDrop) {
         onDrop.call(this, acceptedFiles, rejectedFiles, evt)
       }
 

src/utils/index.js
@@ -10,16 +10,14 @@ export function getDataTransferItems(event) {
   if (event.dataTransfer) {
     const dt = event.dataTransfer
 
+    // NOTE: Only the 'drop' event has access to DataTransfer.files,
+    // otherwise it will always be empty
     if (dt.files && dt.files.length) {
       dataTransferItemsList = dt.files
     } else if (dt.items && dt.items.length) {
       // During the drag even the dataTransfer.files is null
       // but Chrome implements some drag store, which is accesible via dataTransfer.items
-      // Map the items to File objects,
-      // and filter non-File items
-      // see https://developer.mozilla.org/en-US/docs/Web/API/DataTransferItem/getAsFile
-      const files = Array.prototype.map.call(dt.items, item => item.getAsFile())
-      dataTransferItemsList = Array.prototype.filter.call(files, file => file !== null)
+      dataTransferItemsList = dt.items
     }
   } else if (event.target && event.target.files) {
     dataTransferItemsList = event.target.files
@@ -43,14 +41,19 @@ export function allFilesAccepted(files, accept) {
   return files.every(file => fileAccepted(file, accept))
 }
 
-export function hasFiles(files) {
-  // Allow only files and retun the items as a list of File,
-  // see https://developer.mozilla.org/en-US/docs/Web/API/DataTransferItem for details
+export function isFileList(items) {
+  // Returns true only for items that are File objects or DataTransferItem of kind 'file',
+  // See https://developer.mozilla.org/en-US/docs/Web/API/DataTransferItem for details
   return (
-    Array.isArray(files) &&
-    files.length > 0 &&
-    Array.prototype.every.call(files, file => file instanceof File)
+    Array.isArray(items) &&
+    items.length > 0 &&
+    (Array.prototype.every.call(items, item => item instanceof File) ||
+      Array.prototype.every.call(items, isKindFile))
   )
+}
+
+export function isKindFile(item) {
+  return typeof item === 'object' && item.kind === 'file'
 }
 
 // allow the entire document to be a drag target

styleguide.config.js
@@ -12,6 +12,7 @@ module.exports = {
   serverPort: 8080,
   sections: [
     {
+      name: '',
       content: 'README.md'
     },
     {

src/index.spec.js
@@ -49,10 +49,7 @@ describe('Dropzone', () => {
     nonFileItems = [
       {
         kind: 'string',
-        type: 'text/plain',
-        getAsFile() {
-          return null
-        }
+        type: 'text/plain'
       }
     ]
 

src/utils/index.spec.js
@@ -1,4 +1,4 @@
-import { getDataTransferItems, isIeOrEdge, hasFiles } from './'
+import { getDataTransferItems, isIeOrEdge, isFileList } from './'
 
 const files = [
   {
@@ -21,10 +21,7 @@ const files = [
 const nonFileItems = [
   {
     kind: 'string',
-    type: 'text/plain',
-    getAsFile() {
-      return null
-    }
+    type: 'text/plain'
   }
 ]
 
@@ -38,10 +35,7 @@ const file = new File([json], 'test.json', {
 const fileItems = [
   {
     kind: 'file',
-    type: 'application/json',
-    getAsFile() {
-      return file
-    }
+    type: 'application/json'
   }
 ]
 
@@ -78,20 +72,6 @@ describe('getDataTransferItems', () => {
     const res = getDataTransferItems(event)
     expect(res).toBeInstanceOf(Array)
     expect(res).toHaveLength(1)
-  })
-
-  it('should ignore dataTransfer.items that are not of kind "file"', () => {
-    const event = {
-      target: {
-        files: [{}]
-      },
-      dataTransfer: {
-        items: nonFileItems
-      }
-    }
-    const res = getDataTransferItems(event)
-    expect(res).toBeInstanceOf(Array)
-    expect(res).toHaveLength(0)
   })
 
   it('should use event.target if dataTransfer is not defined', () => {
@@ -158,11 +138,12 @@ describe('isIeOrEdge', () => {
   })
 })
 
-describe('hasFiles', () => {
-  it('should only return true for an Array of File objects', () => {
-    expect(hasFiles([file])).toBe(true)
-    expect(hasFiles(['domNode'])).toBe(false)
-    expect(hasFiles([])).toBe(false)
-    expect(hasFiles(null)).toBe(false)
+describe('isFileList', () => {
+  it('should only return true for an Array of File objects or DataTransferItem of kind file', () => {
+    expect(isFileList([file])).toBe(true)
+    expect(isFileList(fileItems)).toBe(true)
+    expect(isFileList(nonFileItems)).toBe(false)
+    expect(isFileList([])).toBe(false)
+    expect(isFileList(null)).toBe(false)
   })
 })